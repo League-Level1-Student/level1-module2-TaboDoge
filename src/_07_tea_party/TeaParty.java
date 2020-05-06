07d1ba61e2d733491750535b6eb95cc2155a82c3
TaboDoge <lucasybarron@gmail.com>
2020-05-05 17:31:04 -0700

    end

--- src/_07_tea_party/TeaParty.java
+++ src/_07_tea_party/TeaParty.java
@@ -2,7 +2,19 @@ package _07_tea_party;
 
 public class TeaParty {
         public String welcome(String name, boolean isWoman, boolean isKnighted) {
-        
+        	if(isWoman == true) {
+        		if(isKnighted == true) {
+			return "Hello Lady " + name;
+        		}
+        		return "Hello Ms. " + name;
+        	}
+        	if(isWoman == false) {
+        		if(isKnighted == true) {
+        			return "Hello Sir " +name;
+        		}
+        		return "Hello Mr. " +name;
+        	}
+			return null;
         }
 }
 
